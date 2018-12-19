7175ebca56063c7cf3764eae71472d6c73a69bad
Dimitris Lamprinos <pkakelas@gmail.com>
2018-12-19 15:07:52 +0200

    Add post, and basic get routes

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'tweets_db'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/' + app.config['MONGO_DBNAME']
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+mongo = PyMongo(app)
+
+@app.route('/tweets', methods=['GET'])
+def tweets():
+    tweets_db = mongo.db.tweets
+    output = []
+
+    for tweet in tweets_db.find():
+        output.append({
+            'user' : tweet['user'],
+            'message' : tweet['message'],
+            'age': tweet['age']
+        })
+
+    return jsonify({'result' : output})
+
+@app.route('/star/hashtag', methods=['GET'])
+def get_hashtagged_tweets(hashtag):
+    tweets_db = mongo.db.tweets
+    regex = ".*" + hashtag + ".*"
+
+    tweets = tweets_db.find({'message': {
+        "$regex": regex
+    }})
+
+    if tweets:
+        output = tweets
+    else:
+        output = "No such tweet"
+    return jsonify({'result' : output})
+
+@app.route('/post', methods=['POST'])
+def post():
+    tweet = request.get_json()
+
+    if tweet.get('user', None) is None or tweet.get('message', None) is None or tweet.get('age', None) is None:
+        return jsonify({"ok": False, "message": "Bad parameters"}), 400
+
+    print(tweet)
+    mongo.db.tweets.insert_one({
+        'user' : tweet['user'],
+        'message' : tweet['message'],
+        'age': tweet['age']
+    })
+
+    return jsonify(tweet)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
