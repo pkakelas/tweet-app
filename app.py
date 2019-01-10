65d0b2727347583f81c6b0b2a3d1ded6d7a80bdd
Dimitris Lamprinos <pkakelas@gmail.com>
2019-01-10 21:04:07 +0200

    Refactoring

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
+from customjson import JSONEncoder
 
 app = Flask(__name__)
 
@@ -10,50 +11,89 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/' + app.config['MONGO_DBNAM
 
 mongo = PyMongo(app)
 
+
 @app.route('/tweets', methods=['GET'])
 def tweets():
+    more_than = request.args.get('morethan') #if key doesn't exist, returns None
     tweets_db = mongo.db.tweets
-    output = []
+    output = {}
+    count = 1
 
     for tweet in tweets_db.find():
-        output.append({
-            'user' : tweet['user'],
-            'message' : tweet['message'],
-            'age': tweet['age']
-        })
+        if more_than != None:
+            if len(tweet['entities']['hashtags']) < int(more_than):
+                continue
 
-    return jsonify(output)
+        output['tweet_' + str(count)] = tweet
+        count += 1
 
-@app.route('/tweets/hashtag', methods=['GET'])
+    if output == {}:
+        return jsonify({"ok": False, "message": "No tweets found"}), 404
+
+    return JSONEncoder().encode(output)
+
+@app.route('/tweets/hashtag/<hashtag>', methods=['GET'])
 def get_hashtagged_tweets(hashtag):
     tweets_db = mongo.db.tweets
-    regex = ".*" + hashtag + ".*"
+    output = {}
+    count = 1
 
-    tweets = tweets_db.find({'message': {
-        "$regex": regex
-    }})
+    for tweet in tweets_db.find():
+        if 'entities' not in tweet:
+            continue
 
-    if tweets:
-        output = tweets
-    else:
-        output = "No such tweet"
-    return jsonify({'result' : output})
+        for candidate in tweet['entities']['hashtags']:
+            if hashtag == candidate['text']:
+                output['tweet_' + str(count)] = tweet
+                count += 1
+
+    if output == {}:
+        return jsonify({"ok": False, "message": "No tweets found"}), 404
+
+    return JSONEncoder().encode(output)
 
 @app.route('/post', methods=['POST'])
 def post():
-    tweet = request.get_json()
+    tweet = request.json
+
+    if tweet == None:
+        return jsonify({"ok": False, "message": "Possibly wrong Content-Type"}), 400
 
     if tweet.get('user', None) is None or tweet.get('message', None) is None or tweet.get('age', None) is None:
         return jsonify({"ok": False, "message": "Bad parameters"}), 400
 
-    print(tweet)
     mongo.db.tweets.insert_one({
         'user' : tweet['user'],
         'message' : tweet['message'],
         'age': tweet['age']
     })
 
-    return jsonify(tweet)
+    return jsonify(tweet), 201
+
+@app.route('/tweets/hashtag/<hashtag>', methods=['DELETE'])
+def delete_hashtagged_tweets(hashtag):
+    tweets_db = mongo.db.tweets
+    to_be_removed = []
+
+    for tweet in tweets_db.find():
+        if 'entities' not in tweet:
+            continue
+
+        for candidate in tweet['entities']['hashtags']:
+            if hashtag == candidate['text']:
+                to_be_removed.append(tweet['_id'])
+
+    tweets_db.delete_many({
+        '_id': {
+            '$in': to_be_removed
+        }
+    })
+
+    return jsonify({'removedCount': len(to_be_removed)})
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'ok': False, 'message': 'Wrong endpoint'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ customjson.py
@@ -0,0 +1,12 @@
+import json
+from bson import ObjectId
+import datetime
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return o.__str__()
+
+        return json.JSONEncoder.default(self, o)
